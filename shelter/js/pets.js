dec0a69299876339412a22e1ecf5bb2008266a79
Belastotski <di.belost@gmail.com>
2022-04-23 01:16:07 +0300

    fix: add json file

--- shelter/js/pets.js
+++
@@ -1,7 +0,0 @@
-let pets = new Array(5);
-for (let id = -20; id != 20 ; id++) {
-console.log(id, id > -1 && id < pets.length? id : (id - (id < 0 ? -1 : 1)*pets.length*(Math.floor(id/pets.length))))
-
-}
-
-
